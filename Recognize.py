3ca561fac2ced1a4261cb6234c12ebe70f9a049b
ashreetp <58861357+ashreetp@users.noreply.github.com>
2020-05-31 10:10:22 +0530

    Add files via upload

---
+++ Recognize.py
@@ -0,0 +1,31 @@
+
+
+from imageai.Prediction.Custom import CustomImagePrediction
+
+import os
+
+
+
+execution_path = os.getcwd()
+
+
+
+prediction = CustomImagePrediction()
+
+prediction.setModelTypeAsResNet()
+
+prediction.setModelPath("Identify.h5")
+
+prediction.setJsonPath("Identify.json")
+
+
+prediction.loadModel(num_objects=10)
+
+
+predictions, probabilities = prediction.predictImage("test.jpg", result_count=3)
+
+
+
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+
+    print(eachPrediction , " : " , eachProbability)
